d0fa6d65f6e24a5389d74e2cab410b97a87e1443
Thiago Campezzi <campezzi@gmail.com>
2016-10-28 11:16:03 +1100

    make it look less hideous

--- Main.elm
+++ Main.elm
@@ -124,20 +124,43 @@ calculateOutcome myChoice theirChoice =
 view : Model -> Html Msg
 view model =
     div []
-        [ div []
-            [ button [ onClick (MyChoice Rock) ] [ text "ME - Rock" ]
-            , button [ onClick (MyChoice Paper) ] [ text "ME - Paper" ]
-            , button [ onClick (MyChoice Scissors) ] [ text "ME - Scissors" ]
-            , button [ onClick (MyChoice Lizard) ] [ text "ME - Lizard" ]
-            , button [ onClick (MyChoice Spock) ] [ text "ME - Spock" ]
-            ]
-        , div []
-            [ button [ onClick (TheirChoice Rock) ] [ text "THEM - Rock" ]
-            , button [ onClick (TheirChoice Paper) ] [ text "THEM - Paper" ]
-            , button [ onClick (TheirChoice Scissors) ] [ text "THEM - Scissors" ]
-            , button [ onClick (TheirChoice Lizard) ] [ text "THEM - Lizard" ]
-            , button [ onClick (TheirChoice Spock) ] [ text "THEM - Spock" ]
-            ]
+        [ h2 [] [ text (outcomeString model.outcome) ]
         , div [] [ button [ onClick Reset ] [ text "RESET" ] ]
+        , hr [] []
+        , div [] [ h3 [] [ text "My Choice" ] ]
+        , div []
+            [ button [ onClick (MyChoice Rock) ] [ text "Rock" ]
+            , button [ onClick (MyChoice Paper) ] [ text "Paper" ]
+            , button [ onClick (MyChoice Scissors) ] [ text "Scissors" ]
+            , button [ onClick (MyChoice Lizard) ] [ text "Lizard" ]
+            , button [ onClick (MyChoice Spock) ] [ text "Spock" ]
+            ]
+        , div [] [ h3 [] [ text "Their Choice" ] ]
+        , div []
+            [ button [ onClick (TheirChoice Rock) ] [ text "Rock" ]
+            , button [ onClick (TheirChoice Paper) ] [ text "Paper" ]
+            , button [ onClick (TheirChoice Scissors) ] [ text "Scissors" ]
+            , button [ onClick (TheirChoice Lizard) ] [ text "Lizard" ]
+            , button [ onClick (TheirChoice Spock) ] [ text "Spock" ]
+            ]
+        , hr [] []
         , text (toString model)
         ]
+
+
+outcomeString : Maybe Outcome -> String
+outcomeString outcome =
+    case outcome of
+        Nothing ->
+            "Game in progress"
+
+        Just result ->
+            case result of
+                Draw ->
+                    "It's a draw!"
+
+                Victory ->
+                    "You win!"
+
+                Defeat ->
+                    "You lose!"
